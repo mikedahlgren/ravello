5050d064491c0b1f194a2a1b5b21cec1164d2d0e
Ryan Hennessy <hennessy@redhat.com>
2014-10-28 21:37:28 -0500

    The inital script for creating and querying instances for the PWOB labs

---
+++ ravello_pwob.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+from ravello_sdk import *
+import argparse
+import getpass
+
+def connectToRavello(args, passwd):
+        client = RavelloClient()
+        client.connect()
+        try:
+                client.login(args.user, passwd)
+        except:
+                print "Invalid Password"
+                quit()
+	return client
+
+
+def findHostnames(args, passwd):
+	client = connectToRavello(args, passwd) 
+
+	apps = client.get_applications({"blueprintName" : args.blueprint})
+
+	fp = open("pwob_labs.csv", "w")
+
+	c=1
+	for single_app in apps:
+	   file_line = "Student " + str(c) + "," 
+	   single_app = client.get_application(single_app['id'])
+	   for vm in single_app['deployment']['vms']:
+	      try: 
+		 name = vm['externalFqdn']
+		 file_line = file_line + name + ","
+	      except:
+		pass 
+	   fp.write(file_line + "\n")
+	   c+=1
+
+	fp.close()
+
+
+def createRavelloApps(args, passwd):
+	client = connectToRavello(args, passwd)
+
+	for c in range(1, args.count+1):
+		app_name = "PWOB-Chi-Student" + str(c).zfill(3) 	
+		new_app=client.create_application({'name': app_name, 'description': 'Platform Without BoundariesLabs for Chicago', 'baseBlueprintId': 51839036})
+		client.set_application_expiration(new_app['id'], {'expirationFromNowSeconds' : '14400' })
+	#	client.publish_application(new_app['id'], {'optimizationLevel': 'COST_OPTIMIZED'})
+
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-b", "--blueprint", type=str, help="Ravello Blue Print to use")
+	parser.add_argument("-u", "--user", type=str, help="Ravello User")
+#	parser.add_argument("-a", "--app-name", type=str, help="Template of application name")
+	parser.add_argument("-c", "--create", action="store_true", help="Create Ravello applications")
+	parser.add_argument("-q", "--query", action="store_true", help="Query VM Names for existing applications")	
+	parser.add_argument("-n", "--count", type=int, help="Number of Application Instances to create")
+	args = parser.parse_args()
+
+	if args.user == None:
+		print "Error: Please specify a Ravello user to use"
+		quit()
+	
+	passwd = getpass.getpass("Ravello Password: ")
+
+	if args.query == True:
+		if args.blueprint == None:
+			print "Error: Please specifiy a Ravello Blueprint to use"
+			quit()	
+		findHostnames(args, passwd)
+
+	if args.create == True:
+		if args.count == None:
+			print "Error: Need the count of applications to create"
+			quit()
+		createRavelloApps(args, passwd)
+
